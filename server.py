f6e4916e35fdf4605c5fa53aa071edc33fa1b8da
Josh Zheng <denial@gmail.com>
2016-10-25 02:28:53 -0700

    updated port to look for VCAP. still not deplying

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import os
 from watson_visual_recognition_tool import app
 
 def runserver():
-	port = int(os.environ.get('PORT', 3000))
+	port = int(os.environ.get('VCAP_APP_PORT', 3000))
 	app.run(host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
