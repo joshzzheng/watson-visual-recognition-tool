cb2a262b6a06bc7be5869432ba56f10b8d7bfbaa
Josh Zheng <denial@gmail.com>
2016-10-25 11:02:06 -0700

    look for port from VCAP

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from watson_visual_recognition_tool import app
 
 def runserver():
   #port = int(os.getenv('VCAP_APP_PORT', 3000))
-  port = int(os.environ.get('PORT', 3000))
+  port = int(os.getenv('VCAP_APP_PORT', 8080))
   app.run(host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
